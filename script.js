9f8b5b79234dc5159c730cd1ea28f1231af27405
Rafael Marquezini Silva <rafael.marquezini.silva@escola.pr.gov.br>
2025-10-26 23:32:16 +0000

    nem fudendo?

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function acender(){
+    document.getElementById("pinguin").src="9f3e53a4-9e72-4d13-a426-0e904e9cf90e.png"
+    console.log("!")
+}
+function apagar(){
+    document.getElementById("pinguin").src="12e4d5a4-f5c5-43ab-95e3-e92650a653a2.png"
+    console.log("?")
+}
